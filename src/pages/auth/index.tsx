e39dd407d2637c51feae65bef71685f31217c10b
TranQuocToan1996 <tranquoctoan.ce@gmail.com>
2024-01-17 20:00:42 +0700

    Feat: Redirect user from auth -> home

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -3,11 +3,30 @@ import NavBar from "@/components/Navbar/Navbar"
 
 import { useRecoilValue } from "recoil"
 import { authModalAtom } from "@/atom/authModalAtom"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { auth } from "@/firebase/firebase"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 type AuthPageProps = {}
 
 const AuthPage: React.FC<AuthPageProps> = (props: AuthPageProps) => {
+    const router = useRouter()
     const authModal = useRecoilValue(authModalAtom)
+    const [user, loading, error] = useAuthState(auth)
+    useEffect(() => {
+        if (loading) return
+        if (user) {
+            router.push('/')
+        }
+    }, [user, router, loading])
+
+    if (loading) return <p>loading...</p>
+    if (error) {
+        alert(error)
+        return
+    }
+
     return (
         <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
             <div className="mx-auto max-w-7xl">
