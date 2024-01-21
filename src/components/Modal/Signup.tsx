c092796ed175ebd011d99e0ca08ed5709a744f1d
TranQuocToan1996 <tranquoctoan.ce@gmail.com>
2024-01-21 20:53:23 +0700

    Feat: Save user to firestore

--- src/components/Modal/Signup.tsx
+++ src/components/Modal/Signup.tsx
@@ -3,9 +3,11 @@ import { useSetRecoilState } from "recoil"
 import { authModalAtom } from "@/atom/authModalAtom"
 import { ChangeEvent, FormEvent, useEffect, useState } from "react"
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
-import { auth } from "@/firebase/firebase"
+import { auth, firestore } from "@/firebase/firebase"
 import { useRouter } from "next/router";
 import { CgSpinner } from "react-icons/cg";
+import { doc, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 type SignUpProps = {}
 
@@ -24,11 +26,27 @@ const SignUp: React.FC<SignUpProps> = (props: SignUpProps) => {
         e.preventDefault()
         if (!inputs.email || !inputs.password || !inputs.displayName) return alert("Please fill all fields");
         try {
+            toast.loading("Creating your account", { position: "top-center", toastId: "loadingToast" });
             const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password)
             if (!newUser) return
+            const createdAt = Date.now()
+            const userData = {
+                uid: newUser.user.uid,
+                email: newUser.user.email,
+                displayName: inputs.displayName,
+                createdAt: createdAt,
+                updatedAt: createdAt,
+                likedProblems: [],
+                dislikedProblems: [],
+                solvedProblems: [],
+                starredProblems: [],
+            };
+            await setDoc(doc(firestore, "users", newUser.user.uid), userData);
             router.push("/")
-        } catch (error) {
-            alert(error)
+        } catch (error: any) {
+            toast.error(error.message, { position: "top-center" });
+        } finally {
+            toast.dismiss("loadingToast");
         }
     }
 
